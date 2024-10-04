403997176b307fbfbff88daa4a38dfadbe897047
Алмазик Галиев < @42-16>
2024-10-04 15:19:40 +0500

    Добавьте файлы проекта.

---
+++ MyKP работа с БД/Pages/ViewReport.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MyKP_работа_с_БД.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewReport.xaml
+    /// </summary>
+    public partial class ViewReport : Page
+    {
+        public ViewReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
